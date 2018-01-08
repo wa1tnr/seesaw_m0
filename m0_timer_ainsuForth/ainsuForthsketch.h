9680fa97f4b380ba1a81f17067b209de7c10e6c6
wa1tnr <wa1tnr@users.noreply.github.com>
2018-01-08 19:16:27 +0000

    new file:   ainsuForthsketch.h Endemic to the program in its present form and structure. Include.

---
+++ m0_timer_ainsuForth/ainsuForthsketch.h
@@ -0,0 +1,9 @@
+// Thu Jul  6 19:10:43 UTC 2017
+// 0103-a0c-01-
+// 4735-b0a-00-
+
+
+#include "src/kernel/signon.h"
+#include "src/kernel/getline.h"
+#include "src/kernel/interpreter.h"
+#include "src/kernel/stacks.h"
