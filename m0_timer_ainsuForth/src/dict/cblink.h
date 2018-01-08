5478e0f5d6e441861a5d85b362d0f595e375bed9
wa1tnr <wa1tnr@users.noreply.github.com>
2018-01-08 19:19:56 +0000

    new file:   allot.cpp 	new file:   allot.h 	new file:   cblink.cpp 	new file:   cblink.h 	new file:   char.cpp 	new file:   char.h 	new file:   colon_semi.cpp 	new file:   colon_semi.h 	new file:   constant.cpp 	new file:   constant.h 	new file:   dict_entries.cpp 	new file:   dict_entries.h 	new file:   do_loop.cpp 	new file:   do_loop.h 	new file:   dump.cpp 	new file:   dump.h 	new file:   help.cpp 	new file:   help.h 	new file:   iasmd.cpp 	new file:   iasmd.h 	new file:   max_min.cpp 	new file:   max_min.h 	new file:   stack_ops.cpp 	new file:   stack_ops.h 	new file:   store_fetch.cpp 	new file:   store_fetch.h 	new file:   xttoname.cpp 	new file:   xttoname.h The Forth Dictionary - non-kernel definitions.  Essential code.

---
+++ m0_timer_ainsuForth/src/dict/cblink.h
@@ -0,0 +1,12 @@
+// Wed Jun 28 17:53:29 UTC 2017
+// 4735-a0s-01-
+
+// #define HAS_NEO_PIXEL_PIN_8 -- yaffa.h
+
+extern const char cblink_str[]; // = "cblink";
+extern void _cblink(void);
+extern void _mblink(void);
+
+/******************************************************************************/
+/**                                                                          **/
+/******************************************************************************/
