44b418ee1012b0eee6ab506bc87ec9ecf4aab3fc
wa1tnr <wa1tnr@users.noreply.github.com>
2018-01-08 19:12:59 +0000

    new file:   flashDict.h A rare bit of inclusion that seems to be required externally to certain parts of the program (though this was done early on, when things were not as well understood).  Artifact of splitting the upstream source into segmented files.  Use and enjoy.

---
+++ m0_timer_ainsuForth/flashDict.h
@@ -0,0 +1,8 @@
+// Thu Jul  6 19:10:43 UTC 2017
+// 0103-a0c-01-
+// 4735-b0a-00-
+
+extern cell_t dStack_pop(void);
+extern const flashEntry_t* pFlashEntry; // = flashDict;
+extern cell_t* pHere;
+
